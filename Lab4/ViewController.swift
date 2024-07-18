619c932609e3199286b1e253b8f1ea8788d9b20c
Yuxuan(Lucien) Liu <70579484+Luciiiiiiiiin@users.noreply.github.com>
2024-07-17 23:59:50 -0500

    Initial Commit

---
+++ Lab4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab4
+//
+//  Created by Yuxuan Liu on 2024/7/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
