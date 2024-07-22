53cd841bedf4f6eabf2a4caef47ef2f49248f6b8
Yuxuan(Lucien) Liu <70579484+Luciiiiiiiiin@users.noreply.github.com>
2024-07-22 14:40:26 -0500

    Movie section done without catche image

--- Lab4/MovieCell.swift
+++ Lab4/MovieCell.swift
@@ -8,23 +8,122 @@
 import UIKit
 
 class MovieCell: UICollectionViewCell {
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
+    static let imageCache = NSCache<NSString, UIImage>()
     
-    func configure(with movie: Movie) {
-        titleLabel.text = movie.title
-        if let posterPath = movie.poster_path {
-            let urlString = "https://image.tmdb.org/t/p/w500\(posterPath)"
-            if let url = URL(string: urlString) {
-                URLSession.shared.dataTask(with: url) { data, response, error in
-                    if let data = data, let image = UIImage(data: data) {
-                        DispatchQueue.main.async {
-                            self.imageView.image = image
-                        }
-                    }
-                }.resume()
-            }
-        }
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.textColor = .white
+        label.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // Add border to the cell
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 1.0
+        
+        contentView.addSubview(imageView)
+        imageView.addSubview(titleLabel)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            // Fill the image inside the MovieCell
+            imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 150), // Fixed width for image view
+            imageView.heightAnchor.constraint(equalToConstant: 250), // Fixed height for image view
+            
+            // Set label at the bottom of the imageView
+            titleLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor, constant: 0),
+            titleLabel.trailingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 0),
+            titleLabel.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 0)
+        ])
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//    func configure(with movie: Movie) {
+//        titleLabel.text = movie.title
+//        if let posterPath = movie.poster_path {
+//            let urlString = "https://image.tmdb.org/t/p/w500\(posterPath)"
+//            if let url = URL(string: urlString) {
+//                URLSession.shared.dataTask(with: url) { data, response, error in
+//                    if let data = data, let image = UIImage(data: data) {
+//                        DispatchQueue.main.async {
+//                            self.imageView.image = image
+//                        }
+//                    }
+//                }.resume()
+//            }
+//        }
+//    }
+    
+//    func configure(with movie: Movie) {
+//        titleLabel.text = movie.title
+//        if let posterPath = movie.poster_path {
+//            let urlString = "https://image.tmdb.org/t/p/w500\(posterPath)" as NSString
+//            
+//            // Check if the image is cached
+//            if let cachedImage = MovieCell.imageCache.object(forKey: urlString) {
+//                imageView.image = cachedImage
+//            } else {
+//                // If not cached, download the image
+//                if let url = URL(string: urlString as String) {
+//                    URLSession.shared.dataTask(with: url) { data, response, error in
+//                        if let data = data, let image = UIImage(data: data) {
+//                            // Cache the image
+//                            MovieCell.imageCache.setObject(image, forKey: urlString)
+//                            DispatchQueue.main.async {
+//                                self.imageView.image = image
+//                            }
+//                        }
+//                    }.resume()
+//                }
+//            }
+//        }
+//    }
+    func configure(with movie: Movie) {
+            titleLabel.text = movie.title
+            if let posterPath = movie.poster_path {
+                let urlString = "https://image.tmdb.org/t/p/w500\(posterPath)" as NSString
+                
+                // Check if the image is cached
+                if let cachedImage = MovieCell.imageCache.object(forKey: urlString) {
+                    print("Image fetched from cache for URL: \(urlString)")
+                    imageView.image = cachedImage
+                } else {
+                    // If not cached, download the image
+                    if let url = URL(string: urlString as String) {
+                        URLSession.shared.dataTask(with: url) { data, response, error in
+                            if let data = data, let image = UIImage(data: data) {
+                                // Cache the image
+                                MovieCell.imageCache.setObject(image, forKey: urlString)
+                                DispatchQueue.main.async {
+                                    self.imageView.image = image
+                                }
+                                print("Image downloaded and cached for URL: \(urlString)")
+                            } else {
+                                print("Failed to create image from data for URL: \(urlString)")
+                            }
+                        }.resume()
+                    }
+                }
+            }
+        }
 }
--- Lab4/ViewController.swift
+++ Lab4/ViewController.swift
@@ -24,18 +24,36 @@ struct Movie: Codable {
     let vote_count: Int
 }
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
-    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var movies: [Movie] = []
-    
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print("Number of items: \(movies.count)") // Debug statement
-        return movies.count
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        searchBar.delegate = self
+        
+        // Register the custom cell programmatically
+        collectionView.register(MovieCell.self, forCellWithReuseIdentifier: "MovieCell")
+        
+        // Set up the collection view layout
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.itemSize = CGSize(width: 150, height: 250) // Fixed item size
+            layout.minimumInteritemSpacing = 10
+            layout.minimumLineSpacing = 20
+            layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        }
+        
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCell", for: indexPath) as! MovieCell
@@ -44,61 +62,50 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         return cell
     }
     
+    // MARK: - UICollectionViewDelegateFlowLayout
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
-//        collectionView.register(UINib(nibName: "MovieCell", bundle: nil), forCellWithReuseIdentifier: "MovieCell")
-        
-        fetchMovies(query: "inception") { result in
-            switch result {
-            case .success(let movies):
-                self.movies = movies
-                DispatchQueue.main.async {
-                    print("Movies fetched: \(movies.count)") // Debug statement
-                    self.collectionView.reloadData()
-                }
-            case .failure(let error):
-                print("Error fetching movies: \(error.localizedDescription)")
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: 250) // Fixed item size
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let query = searchBar.text, !query.isEmpty else { return }
+        fetchMovies(query: query)
+    }
+    
+    func fetchMovies(query: String) {
+            let apiKey = "bb619457f11db06fe46774f6a24882b3"
+            let urlString = "https://api.themoviedb.org/3/search/movie?api_key=\(apiKey)&query=\(query)&page=1&per_page=20"
+            
+            guard let url = URL(string: urlString) else {
+                print("Invalid URL")
+                return
             }
+            
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print("Error fetching movies: \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let data = data else {
+                    print("No data")
+                    return
+                }
+                
+                do {
+                    let decoder = JSONDecoder()
+                    let apiResults = try decoder.decode(APIResults.self, from: data)
+                    self.movies = apiResults.results
+                    DispatchQueue.main.async {
+                        self.collectionView.reloadData()
+                    }
+                } catch {
+                    print("Error decoding data: \(error.localizedDescription)")
+                }
+            }
+            
+            task.resume()
         }
-    }
-
-
 }
 
-func fetchMovies(query: String, completion: @escaping (Result<[Movie], Error>) -> Void) {
-    let apiKey = "bb619457f11db06fe46774f6a24882b3"
-    let urlString = "https://api.themoviedb.org/3/search/movie?api_key=\(apiKey)&query=\(query)"
-    
-    guard let url = URL(string: urlString) else {
-        completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
-        return
-    }
-    
-    let task = URLSession.shared.dataTask(with: url) { data, response, error in
-        if let error = error {
-            completion(.failure(error))
-            return
-        }
-        
-        guard let data = data else {
-            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data"])))
-            return
-        }
-        
-        do {
-            let decoder = JSONDecoder()
-            let apiResults = try decoder.decode(APIResults.self, from: data)
-            completion(.success(apiResults.results))
-        } catch {
-            completion(.failure(error))
-        }
-    }
-    
-    task.resume()
-}
